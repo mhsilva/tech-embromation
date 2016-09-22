f025a5378ce8ab048009b8732894f41e13bd1d9b
Matheus Silva <m.silva@thomsonreuters.com>
2016-09-22 18:09:43 -0300

    Change URL

--- src/main/java/com/techinicalembromation/controller/TeFrases.java
+++ src/main/java/com/techinicalembromation/controller/TeFrases.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techinicalembromation.util.TeFraseBuilder;
 
 @RestController
-@RequestMapping(value = "/frases")
+@RequestMapping(value = "/")
 public class TeFrases {
 
 	@Autowired
