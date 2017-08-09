4707815876f0a53ee2d39bf93f5a74fb32e355bb
Matheus Silva <matheus.silva@thomsonreuters.com>
2017-08-08 23:01:58 -0300

    Add CORS on getFrase

--- src/main/java/com/techinicalembromation/controller/TeFrases.java
+++ src/main/java/com/techinicalembromation/controller/TeFrases.java
@@ -1,6 +1,7 @@
 package com.techinicalembromation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ public class TeFrases {
 	@Autowired
 	private TeFraseBuilder builder;
 
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody String getFrase() {
 		return builder.constroiFrase();
